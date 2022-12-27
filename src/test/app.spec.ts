a96fb58efac31763988a2b5d5d02e1b13cb26284
drAbdelrahmanGamal <dr.abdo2022@outlook.com>
2022-12-27 11:11:57 +0200

    simplifying the test code for endpoints

--- src/test/app.spec.ts
+++ src/test/app.spec.ts
@@ -1,14 +1,16 @@
 import supertest from 'supertest';
 import app from '../app';
 
+// eslint-disable-next-line
+export const testEnd = (done: DoneFn) => {
+  return (err: Error): void => {
+    if (err) done.fail(err);
+    else done();
+  };
+};
+
 describe('Test server up and running', (): void => {
   it('should run server on localhost correctly with status: 200', (done): void => {
-    supertest(app)
-      .get('/')
-      .expect(200)
-      .end((err: Error) => {
-        if (err) done.fail(err);
-        else done();
-      });
+    supertest(app).get('/').expect(200, testEnd(done));
   });
 });
--- src/test/routes.spec.ts
+++ src/test/routes.spec.ts
@@ -1,59 +1,34 @@
 import supertest from 'supertest';
 import app from '../app';
+import { testEnd } from './app.spec';
 
 describe('Test app endpoints', (): void => {
   describe('Test main route endpoint', (): void => {
     it('should return 200 on /', (done): void => {
-      supertest(app)
-        .get('/')
-        .expect(200)
-        .end((err: Error) => {
-          if (err) done.fail(err);
-          else done();
-        });
+      supertest(app).get('/').expect(200, testEnd(done));
     });
   });
 
   describe('Test images route endpoints', (): void => {
     it('should return 200 on /images', (done): void => {
-      supertest(app)
-        .get('/images')
-        .expect(200)
-        .end((err: Error) => {
-          if (err) done.fail(err);
-          else done();
-        });
+      supertest(app).get('/images').expect(200, testEnd(done));
     });
 
     describe('Test images/resize route endpoint', (): void => {
       it('should return 400 on /images/resize', (done): void => {
-        supertest(app)
-          .get('/images/resize')
-          .expect(400)
-          .end((err: Error) => {
-            if (err) done.fail(err);
-            else done();
-          });
+        supertest(app).get('/images/resize').expect(400, testEnd(done));
       });
 
       it('should return 400 on /images/resize/?w=400&h=400', (done): void => {
         supertest(app)
           .get('/images/resize/?w=400&h=400')
-          .expect(400)
-          .end((err: Error) => {
-            if (err) done.fail(err);
-            else done();
-          });
+          .expect(400, testEnd(done));
       });
 
       it('should return 200 on /images/resize/?i=01.png&w=400&h=400', (done): void => {
         supertest(app)
           .get('/images/resize/?i=01.png&w=400&h=400')
-          .expect(200)
-          .end((err: Error) => {
-            if (err) done.fail(err);
-            else done();
-          });
+          .expect(200, testEnd(done));
       });
     });
   });
